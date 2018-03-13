fa8ff2e4b57dd04485364bc73320dc87049502e4
Jin <celine.jkim@gmail.com>
2018-03-12 22:30:31 -0400

    README.md added

---
+++ assets/javascript/fizzbuzz.js
@@ -0,0 +1,19 @@
+for (var x=1; x <= 100; x++) {
+    var skip = 0;
+    if (x % 3 == 0) {
+        document.write('fizz');
+        skip = 1;
+    }
+    if (x % 5 == 0) {
+        document.write('buzz');
+        skip = 1;
+    }
+    if (x % 15 == 0) {
+        document.write('fizzbuzz');
+        skip = 1;
+    }
+    if (!skip) {
+        document.write(x);
+    }
+    document.write('<br>'); //line breaks to enhance output readability
+}
